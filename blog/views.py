84cfb7b1e70d1fc3666b33e4e8c85aade22e0fc4
Peyman Hassani <hassani.peyman627@gmail.com>
2020-11-30 04:42:41 +0330

    blog's ordred

--- blog/views.py
+++ blog/views.py
@@ -4,6 +4,6 @@ from .models import Blog
 
 
 def all_blogs(request):
-    blogs = Blog.objects.all()
+    blogs = Blog.objects.order_by('-date')
     context = {'blogs': blogs}
     return render(request, 'blog/all_blogs.html', context)
