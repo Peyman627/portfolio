932e5351f3559f9d234ce4e932344b2dadecbd81
Peyman Hassani <hassani.peyman627@gmail.com>
2020-11-30 14:10:22 +0330

    urls for apps polished

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 from .models import Blog
 
@@ -10,6 +10,6 @@ def all_blogs(request):
 
 
 def detail(request, blog_id):
-    blog = Blog.objects.get(pk=blog_id)
+    blog = get_object_or_404(Blog, pk=blog_id)
     context = {'blog': blog}
     return render(request, 'blog/detail.html', context)
