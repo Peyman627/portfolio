0f36b7d52ac893556a9de158999693856982afaf
Peyman Hassani <hassani.peyman627@gmail.com>
2020-11-30 04:35:19 +0330

    blog page up and running

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
 
+from .models import Blog
+
 
 def all_blogs(request):
-    return render(request, 'blog/all_blogs.html')
+    blogs = Blog.objects.all()
+    context = {'blogs': blogs}
+    return render(request, 'blog/all_blogs.html', context)
